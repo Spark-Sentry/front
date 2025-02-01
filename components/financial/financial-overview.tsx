763f6e248285b7b84df82acc98ec03c4f8749f2d
RichyGang <rgtid17@gmail.com>
2025-02-01 13:14:13 -0500

    Commit before rebuild

--- components/financial/financial-overview.tsx
+++ components/financial/financial-overview.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
 import { TrendingUp, TrendingDown, Clock, LineChart, ArrowRight, HandCoins } from 'lucide-react';
 import OverviewCard from '@/components/ui/overview-card';
 
